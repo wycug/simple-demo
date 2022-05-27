ca19f44f4fece845740cf707d241f5297d0c3e00
wycug <913680028@qq.com>
2022-05-27 19:26:24 +0800

    5-27 19:26

--- controller/publish.go
+++ controller/publish.go
@@ -15,9 +15,46 @@ type VideoListResponse struct {
 
 // Publish check token then save upload file to public directory
 func Publish(c *gin.Context) {
+	token := c.PostForm("token")
+	title := c.PostForm("title")
+	fmt.Println(token)
+	if _, exist := UserIsExist(token, "", ""); !exist {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+		return
+	}
+	data, err := c.FormFile("data")
+	if err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
+	filename := filepath.Base(data.Filename)
+	user := usersLoginInfo[token]
+	finalName := fmt.Sprintf("%d_%s", user.Id, filename)
+	saveFile := filepath.Join("./public/", finalName)
+	if err := c.SaveUploadedFile(data, saveFile); err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
+	ipport := fmt.Sprintf("%s%s%s/static/", URL, PORT, GROUPPATH)
+	url := ipport + finalName
+	err = dao.CreateVideoInfo(user.Id, url, "", title)
+	if err != nil {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 1,
+			StatusMsg:  err.Error(),
+		})
+		return
+	}
 
-}
+	c.JSON(http.StatusOK, Response{
+		StatusCode: 0,
+		StatusMsg:  finalName + " uploaded successfully",
+	})
 
-// PublishList all users have same publish video list
-func PublishList(c *gin.Context) {
 }
