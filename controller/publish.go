9f8a55a8f61605631a4877b14d57b6d35e05fbdb
yuhao773389665 <773389665@qq.com>
2022-05-28 10:18:37 +0800

    commit infomation

--- controller/publish.go
+++ controller/publish.go
@@ -62,10 +62,11 @@ func Publish(c *gin.Context) {
 
 // PublishList all users have same publish video list
 func PublishList(c *gin.Context) {
-	c.JSON(http.StatusOK, VideoListResponse{
-		Response: Response{
-			StatusCode: 0,
-		},
-		VideoList: DemoVideos,
-	})
+	Feed(c)
+	//c.JSON(http.StatusOK, VideoListResponse{
+	//	Response: Response{
+	//		StatusCode: 0,
+	//	},
+	//	VideoList: DemoVideos,
+	//})
 }
--- dao/feed.go
+++ dao/feed.go
@@ -4,7 +4,8 @@ import "errors"
 
 func GetVideoList(nums int) ([]VideoInfo, error) {
 	var videos []VideoInfo
-	result := db.Table("video_info").Find(&videos).Limit(nums)
+	//Order按创建时间倒叙排序
+	result := db.Table("video_info").Order("created_at desc").Find(&videos).Limit(nums)
 	if result.RowsAffected == 0 {
 		return nil, errors.New("not exist video")
 	}
