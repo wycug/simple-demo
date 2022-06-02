41233ba96b7da1ba82e828b00cb3f7382d7ed3f7
xwj <281658285@qq.com>
2022-06-02 18:02:31 +0800

    update favorite params bind

--- controller/favorite.go
+++ controller/favorite.go
@@ -27,11 +27,15 @@ type FavoriteActionListRequest struct {
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
 	var params FavoriteActionListRequest
-	err := c.Bind(&params)
-	if err != nil {
-		log.Println("bind param fail, err =", err.Error())
-		return
-	}
+	//err := c.Bind(&params)
+	params.Token = c.Query("token")
+	params.UserId = c.Query("user_id")
+	params.VideoId = c.Query("video_id")
+	params.ActionType = c.Query("action_type")
+	//if err != nil {
+	//	log.Println("bind param fail, err =", err.Error())
+	//	return
+	//}
 	fmt.Println(params)
 	if _, exist := UserIsExist(params.Token); exist {
 		if params.ActionType == "1" {
